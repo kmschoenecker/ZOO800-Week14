7dca6903dbff11cd0abdcf8b331dd3c33549e881
kmschoenecker <kschoenecker@wisc.edu>
2025-12-07 17:32:06 -0600

    finished objective 2, a bit confused on the intercept model

--- Week14.R
+++ Week14.R
@@ -39,7 +39,8 @@ ticks$life_stage <- as.factor(ticks$life_stage)
 
 full_model <- lm(questing_duration ~ humidity_index * life_stage, data = ticks)
 
-### A reduced model with no interaction term.
+### A reduced model with no interaction term. But we still want to include life stage
+# so we use the + when making the model, right?
 
 reduced_model <- lm(questing_duration ~ humidity_index + life_stage, data = ticks)
 
@@ -52,10 +53,10 @@ summary(reduced_model)
 # Which has the lower NLL?
 
 NLL_full_model <- logLik(full_model)
-NLL_full_model
+NLL_full_model # -535.2249
 
 NLL_reduced_model <- logLik(reduced_model)
-NLL_reduced_model
+NLL_reduced_model # -535.2963
 
 ## (D) Compare the two models using the likelihood ratio test (lmtest::lrtest) to see whether the NLL of
 # the full model is sufficiently lower to justify the additional parameter. Which model is preferred
@@ -66,7 +67,7 @@ likelihood_ratio_test <- lrtest(full_model, reduced_model)
 likelihood_ratio_test 
 
 # According to the likelihood ratio test, there is not a significant degree of difference
-# between the model. Following the idea of parsimony, we should then used the reduced model.
+# between the models. Following the idea of parsimony, we should then used the reduced model.
 # From this result, we would conclude that the questing duration of ticks is not 
 # determined by their life stage, but solely by the humidity index.
 
@@ -85,6 +86,50 @@ likelihood_ratio_test
 ### d. Single variable 2 – intercept and X2
 ### e. Intercept only
 
-# with the simulated data I have, there is only one x-variable to compare
+# with the simulated data I have, there is only one x-variable to compare?
+# is it fine to also run the models with just life stage, even though that is a 2-level factor
+# and not continous?
+
+# Full model, same as what we used in Objective 1
+full_model
+
+# Main effects, same as our reduced model above? Since we are not including the interaction effect?
+reduced_model
+
+# Single variable 1, so only model based on questing duration and humidity, do not include age?
+
+single_var1_model <- lm(questing_duration ~ humidity_index, data = ticks)
+
+# Single variable 2, model questing duration based on life stage?
+# is this correct to do, with life stage not being continous, in this case?
+
+single_var2_model <- lm(questing_duration ~ life_stage, data = ticks)
+
+# Intercept only? I am a little confused as to what these means
+# Nathan mentioned just setting the intercept to 1?
+
+intercept_model <- lm(questing_duration ~ 1, data = ticks) # not sure this is right
+# r lets me run the model, but I guess that does not mean it is technically correct
+
+# extract our AIC values, there is a function called AIC() in R to do such
+
+aic_full <- AIC(full_model)
+aic_main <- AIC(reduced_model)
+aic_var1 <- AIC(single_var1_model)
+aic_var2 <- AIC(single_var2_model)
+aic_int <- AIC(intercept_model)
+
+# data frame with our AIC values
+
+aic_table <- data.frame(Model = c("Full Model", "Main Effects", "Humidity Only", "Life Stage Only", "Intercept"),
+                        AIC = c(aic_full, aic_main, aic_var1, aic_var2, aic_int))
+
+aic_table #view our table
 
 ## (B) Which model or models are supported by AIC? What does this mean ecologically?
+
+# The Main Effects and Humidity Only model, have the lowest AIC values.
+# with the Full Model still within reason. We should exclude our Life Stage only and Intercept
+# models. The "best" models being the Main Effects and Humidity only again suggests
+# that the interaction between life stage is not a significant driver of the trend we 
+# are seeing.
